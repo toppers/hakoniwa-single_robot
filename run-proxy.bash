f8f9d98a91c2c6d8c6b1199b4e57ff124a242597
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-05-25 13:04:58 +0900

    Successfully built app without ev3rt-athrill-v850e2m in this repo

--- run-proxy.bash
+++ run-proxy.bash
@@ -7,11 +7,11 @@ then
 fi
 
 export APLNAME=${1}
-if [ -d ../sdk/workspace/${APLNAME} ]
+if [ -d ./sdk/workspace/${APLNAME} ]
 then
 	:
 else
-	echo "ERROR: can not found ${APLNAME} on ../sdk/workspace/"
+	echo "ERROR: can not found ${APLNAME} on ./sdk/workspace/"
 	exit 1
 fi
 
@@ -24,13 +24,13 @@ fi
 bash utils/config/mo utils/config/proxy_param_json.mo > proxy/proxy_param.json
 
 
-WORKSPACE_DIR=$(cd .. && pwd)
+WORKSPACE_DIR=$(pwd)
 DOCKER_IMAGE=single-robot/ev3rt-v850:v1.0.0
 
 sudo docker run \
-	-v ${WORKSPACE_DIR}:/root/workspace/ev3rt-athrill-v850e2m \
-	-v ${WORKSPACE_DIR}/docker/utils/config:/root/workspace/config \
-	-v ${WORKSPACE_DIR}/docker/utils/make:/root/workspace/make \
-	-v ${WORKSPACE_DIR}/docker/unity:/root/workspace/unity \
-	-v ${WORKSPACE_DIR}/docker/proxy:/root/workspace/proxy \
+	-v ${WORKSPACE_DIR}/sdk:/root/workspace/sdk \
+	-v ${WORKSPACE_DIR}/utils/config:/root/workspace/config \
+	-v ${WORKSPACE_DIR}/utils/make:/root/workspace/make \
+	-v ${WORKSPACE_DIR}/unity:/root/workspace/unity \
+	-v ${WORKSPACE_DIR}/proxy:/root/workspace/proxy \
 	-it --rm --net host --name ev3rt-v850 ${DOCKER_IMAGE} 
--- utils/clean_build.bash
+++ utils/clean_build.bash
@@ -5,7 +5,7 @@ then
 	echo "Usage: $0 <apl-name>"
 	exit 1
 fi
-EV3RT_DIR=ev3rt-athrill-v850e2m/sdk/workspace
+EV3RT_DIR=./sdk/workspace
 APL_NAME=${1}
 
 cd ${EV3RT_DIR}

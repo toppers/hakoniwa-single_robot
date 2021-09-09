62a1a211fa753d0ca485692142361c8fdb4ff33b
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-09-09 11:20:49 +0900

    use container name to specify exec target

--- docker/attach-docker.bash
+++ docker/attach-docker.bash
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-DOCKER_IMAGE=`cat docker/docker_image.txt`
-DOCKER_ID=`sudo docker ps | grep "${DOCKER_IMAGE}" | awk '{print $1}'`
-
-sudo docker exec -it ${DOCKER_ID} /bin/bash
+sudo docker exec -it hakoniwa_single-robot /bin/bash
